e4509a5b6028dee161230a3f10a990c2746ef35f
Farhad Aman <farhad.aman@snapp.cab>
2023-06-05 01:23:26 +0330

    feat: complete fully connected layer

--- layers/fullyconnected.py
+++ layers/fullyconnected.py
@@ -1,7 +1,8 @@
 import numpy as np
 
+
 class FC:
-    def __init__(self, input_size : int, output_size : int, name : str, initialize_method : str="random"):
+    def __init__(self, input_size: int, output_size: int, name: str, initialize_method: str = "random"):
         self.input_size = input_size
         self.output_size = output_size
         self.name = name
@@ -9,25 +10,20 @@ class FC:
         self.parameters = [self.initialize_weights(), self.initialize_bias()]
         self.input_shape = None
         self.reshaped_shape = None
-    
+
     def initialize_weights(self):
         if self.initialize_method == "random":
-            # TODO: Initialize weights with random values using np.random.randn
-            return None * 0.01
-
+            return np.random.randn(self.output_size, self.input_size)
         elif self.initialize_method == "xavier":
             return None
-
         elif self.initialize_method == "he":
             return None
-
         else:
             raise ValueError("Invalid initialization method")
-    
+
     def initialize_bias(self):
-        # TODO: Initialize bias with zeros
-        return np.zeros((None, 1))
-    
+        return np.zeros((self.output_size, 1))
+
     def forward(self, A_prev):
         """
         Forward pass for fully connected layer.
@@ -37,21 +33,16 @@ class FC:
             returns:
                 Z: output of the fully connected layer
         """
-        # NOTICE: BATCH_SIZE is the first dimension of A_prev
         self.input_shape = A_prev.shape
         A_prev_tmp = np.copy(A_prev)
-
-        # TODO: Implement forward pass for fully connected layer
-        if None: # check if A_prev is output of convolutional layer
-            batch_size = None
-            A_prev_tmp = A_prev_tmp.reshape(None, -1).T
+        if A_prev.ndim == 4:
+            batch_size = A_prev.shape[0]
+            A_prev_tmp = A_prev_tmp.reshape(batch_size, -1).T
         self.reshaped_shape = A_prev_tmp.shape
-        
-        # TODO: Forward part
-        W, b = None
-        Z = None @ None + None
+        W, b = self.parameters[0], self.parameters[1]
+        Z = W @ A_prev_tmp + b
         return Z
-    
+
     def backward(self, dZ, A_prev):
         """
         Backward pass for fully connected layer.
@@ -63,21 +54,18 @@ class FC:
                 grads: list of gradients for the weights and bias
         """
         A_prev_tmp = np.copy(A_prev)
-        if None: # check if A_prev is output of convolutional layer
-            batch_size = None
-            A_prev_tmp = A_prev_tmp.reshape(None, -1).T
-
-        # TODO: backward part
-        W, b = None
-        dW = None @ None.T / None
-        db = np.sum(None, axis=1, keepdims=True) / None
-        dA_prev = None.T @ None
+        if A_prev.ndim == 4:
+            batch_size = A_prev.shape[0]
+            A_prev_tmp = A_prev_tmp.reshape(batch_size, -1).T
+        W, b = self.parameters[0], self.parameters[1]
+        dW = (1 / A_prev.shape[1]) * np.dot(dZ, A_prev_tmp.T)
+        db = (1 / A_prev.shape[1]) * np.sum(dZ, axis=1, keepdims=True)
+        dA_prev = np.dot(W.T, dZ)
         grads = [dW, db]
-        # reshape dA_prev to the shape of A_prev
-        if None:    # check if A_prev is output of convolutional layer
+        if len(self.input_shape) == 4:
             dA_prev = dA_prev.T.reshape(self.input_shape)
         return dA_prev, grads
-    
+
     def update_parameters(self, optimizer, grads):
         """
         Update the parameters of the layer.
