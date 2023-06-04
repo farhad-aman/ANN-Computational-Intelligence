95097bfb62fa3acc40b85b20a5c178e64f5376a8
Farhad Aman <farhad.aman@snapp.cab>
2023-06-05 02:42:38 +0330

    feat: complete GD

--- optimizers/gradientdescent.py
+++ optimizers/gradientdescent.py
@@ -1,15 +1,14 @@
-# TODO: Implement the gradient descent optimizer
 class GD:
     def __init__(self, layers_list: dict, learning_rate: float):
         """
         Gradient Descent optimizer.
             args:
-                layers_list: dictionary of layers name and layer object
+                layers_list: dictionary of layers names and layer object
                 learning_rate: learning rate
         """
         self.learning_rate = learning_rate
         self.layers = layers_list
-    
+
     def update(self, grads, name):
         """
         Update the parameters of the layer.
@@ -19,9 +18,8 @@ class GD:
             returns:
                 params: list of updated parameters
         """
-        layer = self.layers[None]
+        layer = self.layers[name]
         params = []
-        #TODO: Implement gradient descent update
-        for None in range(len(grads)):
-            params.append(None - None * None)
+        for i in range(len(grads)):
+            params.append(layer.parameters[i] - self.learning_rate * grads[i])
         return params
