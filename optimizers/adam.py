9ba315cd37e168af5a30b2d78d8c3c2787d7fa3d
Farhad Aman <farhad.aman@snapp.cab>
2023-06-05 02:46:55 +0330

    feat: complete Adam

--- optimizers/adam.py
+++ optimizers/adam.py
@@ -1,7 +1,6 @@
 import numpy as np
-from .gradientdescent import GradientDescent
 
-# TODO: Implement Adam optimizer
+
 class Adam:
     def __init__(self, layers_list, learning_rate=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8):
         self.layers = layers_list
@@ -11,21 +10,20 @@ class Adam:
         self.epsilon = epsilon
         self.V = {}
         self.S = {}
-        for None in layers_list:
-            # TODO: Initialize V and S for each layer (v and s are lists of zeros with the same shape as the parameters)
-            v = [None for p in layers_list[None].parameters]
-            s = [None for p in layers_list[None].parameters]
-            self.V[None] = v
-            self.S[None] = s
-        
+        for name in layers_list:
+            v = [np.zeros_like(p) for p in layers_list[name].parameters]
+            s = [np.zeros_like(p) for p in layers_list[name].parameters]
+            self.V[name] = v
+            self.S[name] = s
+
     def update(self, grads, name, epoch):
-        layer = self.layers[None]
+        layer = self.layers[name]
         params = []
-        # TODO: Implement Adam update
-        for None in range(len(grads)):
-            self.V[None][None] = None * None + (1 - None) * None
-            self.S[None][None] = None * None  +(1 - None) * np.square(None)
-            self.V[None][None] /= (1 - np.power(self.beta1, epoch)) # TODO: correct V
-            self.S[None][None] /= (1 - np.power(self.beta2, epoch)) # TODO: correct S
-            params.append(None - None * None / (np.sqrt(None) + None))
+        for i in range(len(grads)):
+            self.V[name][i] = self.beta1 * self.V[name][i] + (1 - self.beta1) * grads[i]
+            self.S[name][i] = self.beta2 * self.S[name][i] + (1 - self.beta2) * np.power(grads[i, 2])
+            self.V[name][i] /= 1 - self.beta1 ** epoch
+            self.S[name][i] /= 1 - self.beta2 ** epoch
+            params.append(layer.parameters[i] - self.learning_rate * self.V[name][i] / np.sqrt(
+                self.S[name][i] + self.epsilon))
         return params
